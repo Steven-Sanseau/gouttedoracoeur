7a55c9489ca08a3d79bec5ee3bab527a20e047b8
Steven Sanséau <steste.etsets@gmail.com>
2018-12-13 13:01:18 +0100

    Change Page

---
+++ src/pages/blcok.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IndexPage = () => (
+    <>
+        Site en construction
+    </>
+)
+
+export default IndexPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,75 @@
 import React from 'react'
+import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
+import Layout from '../layouts/layout'
 
-const IndexPage = () => (
-    <>
-        Site en construction
-    </>
+// Components
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import VideoList from '../components/VideoList'
+import Donators from '../components/Donators'
+import Description from '../components/Description'
+
+const IndexPage = ({ data }) => (
+  <>
+    <Layout>
+      <Helmet>
+        <title>Goutte d'or à Coeur le film</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Documentaire Goutte d'or à Coeur" />
+      </Helmet>
+      <Header />
+      <Description />
+      <VideoList videoList={data.videoList.edges[0].node.childDataJson} />
+      <Donators donators={data.donators.edges[0].node.childrenDonatorsJson} />
+      <Footer />
+    </Layout>
+  </>
 )
+
+export const query = graphql`
+  query DonatorQuery {
+    donators: allFile(
+      filter: { name: { eq: "donators" }, sourceInstanceName: { eq: "data" } }
+    ) {
+      edges {
+        node {
+          childrenDonatorsJson {
+            name
+            firstName
+          }
+        }
+      }
+    }
+    videoList: allFile(
+      filter: { name: { eq: "videoList" }, sourceInstanceName: { eq: "data" } }
+    ) {
+      edges {
+        node {
+          childDataJson {
+            sequences {
+              title
+              link
+              img
+              description
+            }
+            meeting {
+              title
+              link
+              img
+              description
+            }
+            talk {
+              title
+              link
+              img
+              description
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
